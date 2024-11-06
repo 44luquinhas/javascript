01feec016e0640bb518e3f271e9c54546ef47a33
44luquinhas <159542226+44luquinhas@users.noreply.github.com>
2024-11-06 09:10:15 -0300

    Add files via upload

---
+++ exer9.js
@@ -0,0 +1,22 @@
+let nome = "Pedroduck";
+console.log("Nome: " + nome);
+let idade = 17;
+console.log("Idade: " + idade);
+let inscrito = true;
+console.log("Está inscrito? " + inscrito);
+let pessoa = {
+    nome: "Pedroduck",
+    idade: 17,
+    inscrito: tru,
+    curso: "técnico em informática"
+};
+
+console.log("Informações sobre a pessoa:");
+console.log(pessoa);
+
+if (pessoa.inscrito) {
+console.log(pessoa.nome + " está inscrita no curso de técnico em informática" + pessoa.curso + ".");
+
+
+
+}
